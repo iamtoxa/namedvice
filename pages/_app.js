a0a78b0e02be31aa6e5f60928e6662f88b8b400d
Anton Medvedev <a.medvedev@pinkman.ru>
2021-05-04 20:02:53 +0300

    brand update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import '../styles/globals.css'
 
+
 const MyApp = React.forwardRef(({ Component, pageProps }, ref)=>{
   useEffect(()=>{
     const calculate = ()=>{
@@ -10,6 +11,27 @@ const MyApp = React.forwardRef(({ Component, pageProps }, ref)=>{
       || document.documentElement.clientHeight
       || document.body.clientHeight;
 
+
+
+      var clientWidth = document.documentElement.clientWidth;
+      var e = 0;
+      const isMobile = clientWidth < 768;
+      const isTablet = !isMobile && clientWidth < 1280;
+      const isDesktop = !isMobile && !isTablet;
+
+      if(isDesktop){
+        e = clientWidth / 1280;
+      } else if (isTablet){
+        e = clientWidth / 768;
+      } else {
+        e = window.innerHeight > window.innerWidth ? clientWidth / 375 : 1
+      }
+
+
+      e = Math.max(6, Math.floor(16 * e));
+
+
+      root.style.setProperty('--base-fontSize', e + "px");
       root.style.setProperty('--client-height', height + "px");
     }
     
