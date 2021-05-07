dcc1855ce32eecc74ed7e32af29a3f6a96c0a185
Anton Medvedev <a.medvedev@pinkman.ru>
2021-05-07 17:42:31 +0300

    fix language change

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -43,14 +43,14 @@ export default function Page({ page, pageLang }) {
 }
 
 export async function getServerSideProps(context) {
-  var pageLang = context.req.headers['accept-language'].split(';')[0].split(',')[0];
+  var pageLang = context.req.headers['accept-language'].split(';')[0].split(',')[0] || 'en';
   try {
     const pageSlug = context.query.slug
       .filter((el) => !["en", "ru"].includes(el))
       .join("/");
 
     pageLang =
-      context.query.slug.find((el) => ["en", "ru"].includes(el)) || context.req.headers['accept-language'].split(';')[0].split(',')[0];
+      context.query.slug.find((el) => ["en", "ru"].includes(el)) || context.req.headers['accept-language'].split(';')[0].split(',')[0] || 'en';
 
     var pageData = require(`Mocks/pages/${pageSlug}`).default;
   } catch (e) {
